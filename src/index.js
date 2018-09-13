f5c93d77338a2af7a05041306d9a609e64f7dd34
Unbelievable <slavlen1999@gmail.com>
2018-09-13 22:07:51 +0300

    sorted

--- src/index.js
+++ src/index.js
@@ -1,31 +1,47 @@
 class Sorter {
-  constructor() {
-    // your implementation
+    constructor() {
+      this.mas = [];
+      this._length = 0;
+      this.compareFunction = function(x,y){return x-y;};
+    }
+  
+    add(element) {
+      this.mas.push(element);
+      this._length++;
+    }
+  
+    at(index) {
+      return this.mas[index];
+    }
+  
+    get length() {
+      return this._length;
+    }
+  
+    toArray() {
+      return this.mas;
+    }
+  
+    sort(indices) {
+      
+      var i=0;
+      var tmp = [];
+      for(i=0; i<indices.length; i++){
+        tmp.push(this.mas[indices[i]]);
+      }
+      
+      tmp.sort(this.compareFunction);
+      
+      indices.sort(function(x,y){return x-y});
+      for(i=0; i<indices.length; i++){
+        this.mas[indices[i]] = tmp[i];
+      }
+      
+    }
+  
+    setComparator(compareFunction) {
+      this.compareFunction = compareFunction;
+    }
   }
-
-  add(element) {
-    // your implementation
-  }
-
-  at(index) {
-    // your implementation
-  }
-
-  get length() {
-    // your implementation
-  }
-
-  toArray() {
-    // your implementation
-  }
-
-  sort(indices) {
-    // your implementation
-  }
-
-  setComparator(compareFunction) {
-    // your implementation
-  }
-}
-
-module.exports = Sorter;
+  
+  module.exports = Sorter;
