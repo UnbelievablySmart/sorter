61ecb4b16ac7b69d294582795c8cf7f5a4b4d46a
Unbelievable <slavlen1999@gmail.com>
2018-10-09 22:06:04 +0300

    perf: improve the code

--- src/index.js
+++ src/index.js
@@ -1,47 +1,48 @@
 class Sorter {
-    constructor() {
-      this.mas = [];
-      this._length = 0;
-      this.compareFunction = function(x,y){return x-y;};
-    }
-  
-    add(element) {
-      this.mas.push(element);
-      this._length++;
-    }
-  
-    at(index) {
-      return this.mas[index];
-    }
-  
-    get length() {
-      return this._length;
-    }
-  
-    toArray() {
-      return this.mas;
-    }
-  
-    sort(indices) {
-      
-      var i=0;
-      var tmp = [];
-      for(i=0; i<indices.length; i++){
-        tmp.push(this.mas[indices[i]]);
-      }
-      
-      tmp.sort(this.compareFunction);
-      
-      indices.sort(function(x,y){return x-y});
-      for(i=0; i<indices.length; i++){
-        this.mas[indices[i]] = tmp[i];
-      }
-      
-    }
-  
-    setComparator(compareFunction) {
-      this.compareFunction = compareFunction;
-    }
+  constructor() {
+    this.array = [];
+    this._length = 0;
+    this.compareFunction = (x, y) => x - y;
   }
   
-  module.exports = Sorter;
+  add(element) {
+    this.array.push(element);
+    this._length++;
+  }
+  
+  at(index) {
+    return this.array[index];
+  }
+  
+  get length() {
+    return this._length;
+  }
+  
+  toArray() {
+    return this.array;
+  }
+  
+  sort(indices) {
+    let i = 0;
+    const temp = [];
+
+    for (i = 0; i < indices.length; i++) {
+      temp.push(this.array[indices[i]]);
+    }
+      
+    temp.sort(this.compareFunction);
+      
+    indices.sort((x, y) => x - y);
+
+    for (i = 0; i < indices.length; i++) {
+      this.array[indices[i]] = temp[i];
+    }
+      
+  }
+  
+  setComparator(compareFunction) {
+    this.compareFunction = compareFunction;
+  }
+}
+  
+module.exports = Sorter;
